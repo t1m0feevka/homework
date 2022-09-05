20860672c1402bd96767bfe6af3f377ea911b69f
t1m0feevka <timofeystepanchyk@gmail.com>
2022-09-05 21:22:06 +0300

    hw8_25_08

--- hw8_25_08.py
+++ hw8_25_08.py
@@ -6,8 +6,7 @@ print(list1)
 
 print('---------- 2 ----------')
 
-list2 = []
-list2_ = [list2.append(0) for i in range(10)]
+list2 = [0 for i in range(10)]
 print(list2)
 
 print('---------- 3 ----------')
@@ -20,3 +19,12 @@ print('---------- 4 ----------')
 list4 = [random.randint(1, 10) for i in range(15)]
 print(list4)
 
+print('---------- - ----------')
+
+list_letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+list_numbers = ['1','2','3','4','5','6','7','8','9']
+your_password = []
+list_all = list_letters + list_numbers
+for i in range(6):
+    your_password.append(random.choice(list_all))
+print(''.join(your_password))
